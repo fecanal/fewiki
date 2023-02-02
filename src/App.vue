2ec9c94e5ec88b56955e198b64ffe8580a560afe
yunhe <yunhe@bytedance.com>
2023-02-02 15:03:56 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Header from './components/Header.vue'
+
+</script>
+
+<template>
+  <Header></Header>
+  <div>app body</div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <header>
+    <nav>
+      <span>首页</span>
+      <span>超人屋</span>
+      <span>创意屋</span>
+      <span>博客屋</span>
+      <span>咨询屋</span>
+    </nav>
+  </header>
+</template>
+<script setup lang="ts">
+
+</script>
